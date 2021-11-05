16a24801e8811b2f11f8feff5cf6cc1ac6bd232c
zhuzhiwen <zhuzhiwen@fancydigital.com.cn>
2021-11-05 18:02:20 +0800

    timer

---
+++ XDTimer.h
@@ -0,0 +1,16 @@
+//
+//  XDTimer.h
+//  XDBusinessModule
+//
+//  Created by fancyzzw on 2021/11/5.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XDTimer : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XDTimer.m
@@ -0,0 +1,12 @@
+//
+//  XDTimer.m
+//  XDBusinessModule
+//
+//  Created by fancyzzw on 2021/11/5.
+//
+
+#import "XDTimer.h"
+
+@implementation XDTimer
+
+@end
