ba243b4b28e243c671675c8c2245c9dacea4f1fb
zhuzhiwen <zhuzhiwen@fancydigital.com.cn>
2021-11-05 19:01:00 +0800

    target

---
+++ XDBusinessModule/Classes/Target_Business.h
@@ -0,0 +1,16 @@
+//
+//  Target_Business.h
+//  XDBusinessModule
+//
+//  Created by fancyzzw on 2021/11/5.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Target_Business : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XDBusinessModule/Classes/Target_Business.m
@@ -0,0 +1,15 @@
+//
+//  Target_Business.m
+//  XDBusinessModule
+//
+//  Created by fancyzzw on 2021/11/5.
+//
+
+#import "Target_Business.h"
+
+@implementation Target_Business
+- (UIViewController *)Action_BusinessViewController{
+    
+    return  [[UIViewController alloc] init];
+}
+@end
