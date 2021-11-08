b8bd94a3925b41371e137a411643e56b0a6d74b8
zhuzhiwen <zhuzhiwen@fancydigital.com.cn>
2021-11-08 11:49:02 +0800

    0.1.4

--- XDBusinessModule/Classes/Target_Business.m
+++ XDBusinessModule/Classes/Target_Business.m
@@ -8,8 +8,12 @@
 #import "Target_Business.h"
 
 @implementation Target_Business
-- (UIViewController *)Action_BusinessViewController{
-    
+- (UIViewController *)Action_BusinessViewController:(NSDictionary *)params{
+    typedef  void (^CallBackType)(NSString*);
+    CallBackType callback = params[@"callback"];
+    if(callback){
+        callback(@"回调成功");
+    }
     return  [[UIViewController alloc] init];
 }
 @end
